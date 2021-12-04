8b96742c5c2f420fb70df1816f53e87f43715ada
Jan Schraff <js-171413@hs-weingarten.de>
2021-12-04 15:41:11 +0100

    finished d1t1

---
+++ d1_t1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace d1_t1
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            string inputfile = "input.txt";
+            string path = Path.Combine(Environment.CurrentDirectory, inputfile);
+            int[] measurements = File.ReadAllLines(path).Select(x=>Int32.Parse(x)).ToArray();
+            int last_measurement = Int32.MaxValue;
+            int count = 0;
+            foreach (int value in measurements){
+                if (value > last_measurement)
+                {
+                    count++;
+                }
+                last_measurement = value;
+            }
+
+            Console.WriteLine(count);
+            Thread.Sleep(2000);
+         
+        }
+    }
+}
