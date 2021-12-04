6e8d4ea0eb48cdde19d72b1c33cf3654f645375a
Jan Schraff <js-171413@hs-weingarten.de>
2021-12-04 15:52:59 +0100

    finished d1t2

---
+++ d1t2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace d1t2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputfile = "input.txt";
+            string path = Path.Combine(Environment.CurrentDirectory, inputfile);
+            int[] measurements = File.ReadAllLines(path).Select(x => Int32.Parse(x)).ToArray();
+            int count = 0;
+            for(int i = 0; i < measurements.Length - 3; i++)
+            {
+                if (measurements[i] < measurements[i + 3])
+                    count++;
+            }
+
+            Console.WriteLine(count);
+            Thread.Sleep(2000);
+
+        }
+    }
+}
