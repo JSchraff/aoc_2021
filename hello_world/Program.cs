50e3dc269ae0f953d6c398b9453d24a0007cb02f
Jan Schraff <js-171413@hs-weingarten.de>
2021-12-04 15:09:02 +0100

    added Hello World

---
+++ hello_world/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Thread.Sleep(2000);
+        }
+    }
+}
