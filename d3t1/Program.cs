4a9573fbf308f6dfb0dc7ef774e91cc52915526f
Jan Schraff <js-171413@hs-weingarten.de>
2021-12-04 17:55:09 +0100

    finished d3t1

---
+++ d3t1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace d1_t1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputfile = "input.txt";
+            string path = Path.Combine(Environment.CurrentDirectory, inputfile);
+            string[] lines = File.ReadAllLines(path);
+
+            int inputs = lines.Length;
+            int input_length = lines[0].Length;
+
+
+            int[] ones = new int[input_length];
+
+            foreach (string line in lines)
+            {
+                for (int j = 0; j < input_length; j++)
+                {
+                    char c = line.ToCharArray()[j];
+                    if (c == '1')
+                    {
+                        ones[j]++;
+                    }
+                }
+            }
+
+            string output = "";
+            foreach (int count in ones)
+            {
+                if (count > inputs / 2)
+                {
+                    output += "1";
+                }
+                else
+                {
+                    output += "0";
+                }
+            }
+
+            uint gamma = Convert.ToUInt32(output, 2);
+            uint epsilon = Convert.ToUInt32(new String(output.Select(x => x == '1' ? '0' : '1').ToArray()), 2);
+
+            Console.WriteLine(gamma * epsilon);
+            Thread.Sleep(10000);
+
+        }
+    }
+}
